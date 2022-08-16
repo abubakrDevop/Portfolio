205077426110488f82ea5b4ec36c1f4bbe6c820c
Abubakr <sherzodabubakr@gmail.com>
2022-08-16 23:38:28 +0600

    fixed errors

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import slc from "../styled/MenuItems.module.scss"
 
 function MenuItems(props) {
-  const [isCompleted, setCompleted] = React.useState(props.completed)
-
-  function handleComlete() {
-    setCompleted(prev => !prev)
-  }
-
   return (
     <NavLink 
       to={props.to} 
-      // className={`${slc.link} ${isCompleted && slc.completed}`} 
-      className={({ isActive }) => isActive ? `${slc.completed} ${slc.link}` : slc.link}
-      onClick={handleComlete}
-    >
+      className={({ isActive }) => isActive ? `${slc.completed} ${slc.link}` : slc.linkBox}>
       <p className={slc.icon}>{props.icon}</p>
       <p className={`${slc.text} ${props.active && slc.textActive}`}>{props.title}</p>
     </NavLink>
